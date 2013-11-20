abbf1e508cfc528d13a705d4b96040cc58132045
Ruiyun Zhou <farcryzry@gmail.com>
2013-11-20 14:44:24 -0800

    integration

--- datawise/core/urls.py
+++ datawise/core/urls.py
@@ -10,16 +10,9 @@ result_resource = ResultResource()
 urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name='core/index.html'), name='index'),
     url(r'^index/$', TemplateView.as_view(template_name='core/index.html'), name='index'),
-    url(r'^calendar/$', TemplateView.as_view(template_name='core/calendar.html'), name='calendar'),
-    url(r'^stats/$', TemplateView.as_view(template_name='core/stats.html'), name='stats'),
-    url(r'^form/$', TemplateView.as_view(template_name='core/form.html'), name='form'),
-    url(r'^tables/$', TemplateView.as_view(template_name='core/tables.html'), name='tables'),
-    url(r'^buttons/$', TemplateView.as_view(template_name='core/buttons.html'), name='buttons'),
-    url(r'^interface/$', TemplateView.as_view(template_name='core/interface.html'), name='interface'),
-    url(r'^login/$', TemplateView.as_view(template_name='core/login.html'), name='login'),
-    url(r'^editors/$', TemplateView.as_view(template_name='core/editors.html'), name='editors'),
+    url(r'^mood/$', TemplateView.as_view(template_name='core/mood.html'), name='calendar'),
     url(r'^correlation/$', TemplateView.as_view(template_name='core/correlation.html'), name='correlation'),
-    url(r'^matrix/$', TemplateView.as_view(template_name='core/matrix.html'), name='matrix'),
+    url(r'^planet/$', TemplateView.as_view(template_name='core/planet.html'), name='matrix'),
 
     url(r'^api/', include(result_resource.urls)),
 )
